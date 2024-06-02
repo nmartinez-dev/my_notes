e357dd17527c0e9a57e7e057ffee19be0d28c8c5
Nicolás Martínez <nmartinez.dev@gmail.com>
2024-06-01 23:21:48 -0300

    Agrego validaciones en formulario.

--- app/src/components/card-modal/CardModal.tsx
+++ app/src/components/card-modal/CardModal.tsx
@@ -17,17 +17,107 @@ const CardModal: FC<CardModalInterface> = (props) => {
   const { cards, setUpdateCards } = useContext(Context);
 
   const [title, setTitle] = useState<string>(titleProp);
+  const [titleError, setTitleError] = useState<boolean>(false);
+  const [titleHelperText, setTitleHelperText] = useState<string>('');
   const [description, setDescription] = useState<ReactNode | string>(
     descriptionProp
   );
+  const [descriptionError, setDescriptionError] = useState<boolean>(false);
+  const [descriptionHelperText, setDescriptionHelperText] =
+    useState<string>('');
 
   const resetForm = () => {
     setTitle('');
+    setTitleError(false);
+    setTitleHelperText('');
     setDescription('');
+    setDescriptionError(false);
+    setDescriptionHelperText('');
     setOpenModal(false);
   };
 
+  const validateFields = () => {
+    if (title == '') {
+      setTitleError(true);
+      setTitleHelperText('El título de la nota no puede estar vacío.');
+      return false;
+    }
+
+    if (title.length > 25) {
+      setTitleError(true);
+      setTitleHelperText(
+        'El título de la nota no puede contener más de 25 caracteres.'
+      );
+      return false;
+    }
+
+    if (description == '') {
+      setDescriptionError(true);
+      setDescriptionHelperText(
+        'La descripción de la nota no puede estar vacía.'
+      );
+      return false;
+    }
+
+    if (typeof description == 'string' && description.length > 255) {
+      setDescriptionError(true);
+      setDescriptionHelperText(
+        'La descripción de la nota no puede contener más de 255 caracteres.'
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleChangeTitle = (event: any) => {
+    setTitle(event.target.value);
+
+    if (event.target.value == '') {
+      setTitleError(true);
+      setTitleHelperText('El título de la nota no puede estar vacío.');
+      return;
+    }
+
+    if (event.target.value.length > 25) {
+      setTitleError(true);
+      setTitleHelperText(
+        'El título de la nota no puede contener más de 25 caracteres.'
+      );
+      return;
+    }
+
+    setTitleError(false);
+    setTitleHelperText('');
+  };
+
+  const handleChangeDescription = (event: any) => {
+    setDescription(event.target.value);
+
+    if (event.target.value == '') {
+      setDescriptionError(true);
+      setDescriptionHelperText(
+        'La descripción de la nota no puede estar vacía.'
+      );
+      return;
+    }
+
+    if (event.target.value.length > 255) {
+      setDescriptionError(true);
+      setDescriptionHelperText(
+        'La descripción de la nota no puede contener más de 255 caracteres.'
+      );
+      return;
+    }
+
+    setDescriptionError(false);
+    setDescriptionHelperText('');
+  };
+
   const handleSubmit = () => {
+    let validation = validateFields();
+    if (!validation) return;
+
     let availableCards = cards.map((value) => value);
     let cardToAdd = {
       label: title,
@@ -56,7 +146,9 @@ const CardModal: FC<CardModalInterface> = (props) => {
           id='title'
           label='Título'
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={handleChangeTitle}
+          error={titleError}
+          helperText={titleHelperText}
           style={{ flex: 1, margin: 10 }}
           autoComplete='off'
         />
@@ -64,7 +156,9 @@ const CardModal: FC<CardModalInterface> = (props) => {
           id='description'
           label='Descripción'
           value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          onChange={handleChangeDescription}
+          error={descriptionError}
+          helperText={descriptionHelperText}
           style={{ flex: 1, margin: 10 }}
           autoComplete='off'
           multiline
